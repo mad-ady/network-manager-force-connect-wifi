2355ad602410090afdfec1752f26e38453e226da
Adrian Popa <adrian.popa.gh@gmail.com>
2019-06-25 15:00:54 +0300

    Automatically detect first wifi, improve parsing reliability (less bash), set a custom metric for the wifi connection and send a message via telegram if it exists

--- wlan_watchdog.pl
+++ wlan_watchdog.pl
@@ -3,24 +3,29 @@ use strict;
 use warnings;
 use Sys::Syslog qw(:standard :macros);  # standard functions & macros
 
+my $wifi = `nmcli --fields=DEVICE device | egrep '^wl' | tail -1`;
+chomp $wifi;
+
 openlog("$0", "pid,nofatal,perror", "local0");
 syslog( "info", "Started");
 sub get_wlan_status {
-  my $status =  `nmcli device | grep wlan0 | grep disconnected | wc -l`;
+  my $status =  `nmcli device | grep $wifi | grep disconnected | wc -l`;
   chomp $status;
   return $status;
 }
 
 sub get_current_connected_ap {
-   my $connected_ap=`nmcli device  | grep wlan0 | cut -c '37-' | sed -r 's/\\s+\$//'`;
+   my $connected_ap=`nmcli device  | grep $wifi | cut -c '37-' | sed -r 's/\\s+\$//'`;
    chomp $connected_ap;
    return $connected_ap;
 }
 
-my @configured_ap=`nmcli connection show | grep wifi | cut -c '1-20' | sed -r 's/\\s+\$//'`;
+my @configured_ap=`nmcli --fields=NAME connection show | tail -n +2`;
 chomp @configured_ap;
-my @visible_ap=`nmcli device wifi | grep Infra | cut -c '1-29' | sed -r 's/\\s+\$//' | sed -r 's/^\\s+//'`;
+s/\s+$//g for (@configured_ap);
+my @visible_ap=`nmcli --fields=SSID device wifi | tail +2`;
 chomp @visible_ap;
+s/\s+$//g for (@visible_ap);
 my %configured_ap_hash = map { $_ => 1 } @configured_ap;
 
 syslog("info", "Visible APs: ".join(", ", @visible_ap));
@@ -36,12 +41,23 @@ if(get_wlan_status() == "1"){
 				syslog("warning", "Wifi is still disconnected...");
 			}
 			else{
-				syslog("info", "Wifi successfully connected to ".get_current_connected_ap());
+				my $new_ap = get_current_connected_ap();
+				syslog("info", "Wifi successfully connected to $new_ap");
+
+				sleep(5);
+				#set the metric (prevent loops through docker/anbox)
+				print `ifmetric $wifi 110`;
+				if( -e "/usr/local/bin/telegram-send"){
+					`/usr/local/bin/telegram-send "Wifi successfully connected to $new_ap"`;
+				}
 				#no need to try further
 				exit;
-			}			
+
+			}
+			
 		}
 	}
+
 }
 else{
 	syslog("info", "We are connected to ".get_current_connected_ap().". Nothing to do");
